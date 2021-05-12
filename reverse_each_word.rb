e072328ce8a01dfb8cec2f76046a439acd742e45
Yoshi Mejia <yoshimejia00@gmail.com>
2021-05-12 19:09:15 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,10 +1,8 @@
 require 'pry'
 def reverse_each_word(string)
  new_string = string.split(" ")
-
   modified = new_string.collect do |sentence|
     sentence.reverse
-    #sentence.join
   end
 modified.join(" ")
  #binding.pry
