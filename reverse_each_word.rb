9cf9fa3a3c1b0096cb0880c3ccc127a221ca2ecd
Yoshi Mejia <yoshimejia00@gmail.com>
2021-05-12 18:54:17 +0000

    Done.

---
+++ reverse_each_word.rb
@@ -0,0 +1,12 @@
+#require 'pry'
+def reverse_each_word(string)
+ new_string = string.split(" ")
+
+  new_string.collect do |sentence|
+    sentence.reverse
+    #sentence.join
+  end
+
+#  binding.pry
+
+end
